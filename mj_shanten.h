7b09a54476cdb301c0310f87253bb4f573fa306e
aikiriao <samuraiaiki@gmail.com>
2020-10-27 12:46:51 +0900

    rename include guard.

--- mj_shanten.h
+++ mj_shanten.h
@@ -1,5 +1,5 @@
-#ifndef MJSYANTEN_H_INCLUDED
-#define MJSYANTEN_H_INCLUDED
+#ifndef MJSHANTEN_H_INCLUDED
+#define MJSHANTEN_H_INCLUDED
 
 #include "mj_tile.h"
 #include <stdint.h>
@@ -22,4 +22,4 @@ int32_t MJShanten_CalculateChitoitsuShanten(const struct Tehai *tehai);
 /* 国士無双の向聴数を計算 1で一向聴, 0で聴牌, -1で和了 */
 int32_t MJShanten_CalculateKokushimusouShanten(const struct Tehai *tehai);
 
-#endif /* MJSYANTEN_H_INCLUDED */
+#endif /* MJSHANTEN_H_INCLUDED */
