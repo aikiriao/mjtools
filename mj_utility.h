d2da88d0f3795a24cde502603c888d83afbc9f57
aikiriao <samuraiaiki@gmail.com>
2020-12-17 02:09:14 +0900

    add unused argument manipulator macro.

--- mj_utility.h
+++ mj_utility.h
@@ -3,6 +3,8 @@
 
 /* 静的アサート */
 #define MJUTILITY_STATIC_ASSERT(cond) void static_assetion_failed(char static_assetion_failed[(cond) ? 1 : -1])
+/* 未使用引数警告回避 */
+#define MJUTILITY_UNUSED_ARGUMENT(arg) { if (arg) { ; } }
 /* nの倍数への切り上げ */
 #define MJUTILITY_ROUND_UP(val, n)    ((((val) + ((n) - 1)) / (n)) * (n))
 /* nの倍数への切り捨て */
