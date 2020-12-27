1eb654e7ec3447194996ae1586d79d7b1e22ec0a
aikiriao <samuraiaiki@gmail.com>
2020-12-27 14:57:28 +0900

    add test code.

---
+++ test/test_mj_player_furoman.c
@@ -0,0 +1,34 @@
+#include "test.h"
+
+/* テスト対象のモジュール */
+#include "../mj_player_furoman.c"
+
+/* テストのセットアップ関数 */
+void MJPlayerFuromanTest_Setup(void);
+
+static int MJPlayerFuromanTest_Initialize(void *obj)
+{
+  TEST_UNUSED_PARAMETER(obj);
+  return 0;
+}
+
+static int MJPlayerFuromanTest_Finalize(void *obj)
+{
+  TEST_UNUSED_PARAMETER(obj);
+  return 0;
+}
+
+/* 生成破棄テスト */
+static void MJPlayerFuromanTest_CreateDestroyTest(void *obj)
+{
+  TEST_UNUSED_PARAMETER(obj);
+}
+
+void MJPlayerFuromanTest_Setup(void)
+{
+  struct TestSuite *suite
+    = Test_AddTestSuite("MJ Player Furo-Man Test Suite",
+        NULL, MJPlayerFuromanTest_Initialize, MJPlayerFuromanTest_Finalize);
+
+  Test_AddTest(suite, MJPlayerFuromanTest_CreateDestroyTest);
+}
